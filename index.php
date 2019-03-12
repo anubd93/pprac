ad9fd1cff840f0a662c4689eb23a3d9580b846a5
anubd93 <anubd93@gmail.com>
2019-03-12 20:25:25 +0600

    :initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<! DOCTYPE html>
+<html>
+<head>
+    <title>LOG RES SYS</title>
+    <link rel="stylesheet" href="inc/bootstrap.min.css"/>
+    <script src="inc/jquery.min.js"></script>
+    <script src="inc/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-brand">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">LOG RES SYS</a>
+                </div>
+                <ul class="nav navbar-nav pull-right">
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="profile.php">Log Out</a></li>
+                    <li><a href="login.php">Log In</a></li>
+                    <li><a href="register.php">Res</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">
+
+            </div>
+        </div>
+
+        <div class="well">
+            <h3>
+                WEBSITE
+                <span class="pull-right">Like</span>
+            </h3>
+        </div>
+
+
+    </div>
+</body>
+</html>
+
